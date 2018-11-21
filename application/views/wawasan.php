c81ddfd6a0ddeef39d492b72b5ccae56d25a8d9c
Reza Pahlevi <32995725+rzphlv@users.noreply.github.com>
2018-11-21 21:47:45 +0700

    wawasan
    nambah dikit

--- application/views/wawasan.php
+++ application/views/wawasan.php
@@ -2,7 +2,69 @@
     include 'header.php';
 ?>
 
-<p>wawasan</p>
+<div class="row">
+
+        <div class="kiri">
+
+          <div class="card">
+            <h2>Tokoh Pertanian</h2>
+            <p>&#9829;</p>
+
+
+
+
+            </form>
+
+          </div>
+
+        <div class="card">
+
+            <h2>Hidroponik Solusi Pertanian Modern Masa Depan</h2>
+            <div class="fakeimg" style="height:300px ; width:750px;"><img src="assets/ino1.png"></div>
+            <p>Dosen dan Ahli di bidang Pertanian Politeknik Pertanian Negeri Payakumbuh ini di Payakumbuh, Minggu (4/11), mengatakan hidroponik mampu menghasilkan produksi tanaman lebih terjamin kebebasannya dari hama penyakit yang berasal dari tanah, dapat dijadikan profesi baru sebagai mata pencaharian bagi petani dan masyarakat yang tidak memiliki pekerjaan, meningkatkan pemenuhan sumber gizi keluarga dan masyarakat, jika diusahakan dalam skala besar dapat meningkatkan ekspor produksi hortikultura segar dan berkualitas tinggi sehingga dapat menambah devisa negara.
+
+Menurut dia, hidroponik juga memiliki keuntungan bagi lingkungan sosial karena dapat dijadikan sarana pendidikan dan pelatihan di bidang pertanian modern mulai dari kanak-kanak sampai dengan orang tua, memperindah lingkungan dengan kesan pertanian yang bersih dan sehat dan usaha agribisnis di perkotaan tanpa mencemari lingkungan.
+
+"Hidroponik merupakan teknik atau cara bercocok tanam dengan menempatkan akar tanaman ke dalam air yang ditambahkan dengan campuran nutrien tanpa menggunakan tanah," katanya.
+
+Dijelaskannya, wadah atau media tanam pengganti tanah tersebut terdiri atas bahan mineral, arang sekam padi, sabut kelapa, pasir, batu bata pecah dan serbuk kayu.
+
+Dia menyebutkan, jenis tanaman yang dibudidayakan secara hidroponik meliputi golongan tanaman hortikultura yang terdiri atas sayur, buah, bunga, tanaman hias, tanaman obat-obatan, tanaman pertamanan dan semua jenis tanaman baik tahunan maupun annual (semusim).
+
+Dia memberitahukan,metode penanaman tanaman hidroponik terdiri atas beberapa macam seperti metode kultur air, NFT (Nutrient Film Technique), Flood and Drain, kultur pasir, kultur bahan porous, substrat, kerikil, auto pot, aeroponic, teknik larutan statis dan air.
+
+Hidroponik dapat diusahakan oleh individu baik sebagai hobi dan tujuan komersil sepanjang tahun tanpa mengenal musim di lahan terbuka, dalam ruangan, apartemen dan rumah kaca di daerah pedesaan dan perkotaaan.
+
+Dia menyebutkan, keunggulan hidroponik dibandingkan dengan bercocok tanam menggunakan tanah antara lain pemeliharaan dan budidaya tanaman hidroponik lebih mudah karena tempatnya relatif bersih, media tanaman yang digunakan steril dan tanaman terlindung dari terpaan hujan, serangan hama penyakit relatif kecil, tanaman lebih sehat, vigor, produktivitasnya tinggi, mutu hasil tanaman berkualitas tinggi dan tahan lama serta harga jualnya tinggi.
+
+"Teknik hidroponik sudah digunakan di luar negeri seperti Singapura yang kekurangan lahan tanah untuk bercocok tanam,para angkasawan menanam sayuran secara hidroponik di dalam kapal angkasa luar dan tentara yang menanam tanaman secara hidroponik di atas kapal perang mereka sehingga tidak perlu lagi membawa bekal yang banyak," kata dia. [ant/ian]</p>
+            
+
+            <p>...</p>
+          </div>
+
+        </div>
+
+
+        <div class="kanan">
+          <div class="card">
+            <p><b>TENTANG AGRIKULTUR.id</b><p>
+            <div class="fakeimg" style="height:200px;"><img src="assets/tentang.png"></div>
+            <p>Menyediakan informasi, inovasi, dan konsultasi</p>
+          </div>
+          <div class="card">
+            <h3>Popular Post</h3>
+            <li><a href="#news"><b><font color = "#ff7b7b"> 1 </b> </a></li>
+            <li><a href="#news"><b> <font color = "#ff7b7b"> 2 </b> </a></li>
+            <li><a href="#news"><b><font color = "#ff7b7b"> 3</b> </a></li>
+          </div>
+          <div class="card">
+            <h3>Follow AGRIKULTUR.id</h3>
+            <p>@agrikultur.id</p>
+          </div>
+        </div>
+    </div>
+
 
 <?php
     include 'footer.php';
