0f458bc8334fb0dbd7812834ee23f1244a460178
Reza Pahlevi <32995725+rzphlv@users.noreply.github.com>
2018-11-23 11:39:36 +0700

    inovator
    inovator

--- application/views/inovator.php
+++ application/views/inovator.php
@@ -1,46 +1,44 @@
+<?php
+    include 'header.php';
 
-<div class="row">
+    $koneksi = new mysqli("localhost", "root", "", "agri");
+?>
 
-        <div class="kiri">
+<h2>Wawasan</h2>
 
-          <div class="card">
-            <h2>Tokoh Pertanian</h2>
-            <p>&#9829;</p>
-
-
-
-
-            </form>
-
-          </div>
-
-        <div class="card">
-
-            <h2>Tokoh Pertanian 2</h2>
-            <p>Mengembangkan Pertanian dengan Hidroponik</p>
-            <div class="fakeimg" style="height:300px ; width:750px;"><img src="assets/ino1.png"></div>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          </div>
-
-        </div>
+<table class="table table-bordered">
 
 
-        <div class="kanan">
-          <div class="card">
-            <p><b>TENTANG AGRIKULTUR.id</b><p>
-            <div class="fakeimg" style="height:200px;"><img src="assets/tentang.png"></div>
-            <p>Menyediakan informasi, inovasi, dan konsultasi</p>
-          </div>
-          <div class="card">
-            <h3>Popular Post</h3>
-            <li><a href="#news"><b><font color = "#ff7b7b"> 1 </b> </a></li>
-            <li><a href="#news"><b> <font color = "#ff7b7b"> 2 </b> </a></li>
-            <li><a href="#news"><b><font color = "#ff7b7b"> 3</b> </a></li>
-          </div>
-          <div class="card">
-            <h3>Follow AGRIKULTUR.id</h3>
-            <p>@agrikultur.id</p>
-          </div>
-        </div>
-    </div>
+  <tbody>
+    <?php $nomor=1; ?>
+    <?php $ambil=$koneksi->query("select *from inovator"); ?>
+    <?php while($pecah = $ambil->fetch_assoc()){ ?>
+    <h3><?php echo $pecah['judul_inovator']; ?></h3>
+
+    <img src="<?php echo site_url(); ?>assets/<?php echo $pecah['foto_inovator']; ?>">
+
+    <br> posted : <?php echo $pecah['date_i']; ?></br>
+    <p><?php echo $pecah['isi_i']; ?></p>
+
+      </td>
+      </tr>
+      <br></br>
+
+      <br></br>
+
+      <br></br>
+      <?php $nomor++; ?>
+    <?php } ?>
+
+  </tbody>
+
+</table>
+
+
+
+
+
+
+<?php
+    include 'footer.php';
+?>
